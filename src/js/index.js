b4a38966361dddb44824fc1079dc19a63dad7150
Favour Samuel <sfchidimma@gmail.com>
2021-03-08 12:32:37 +0100

    Include html and styles content

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,1 @@
-$(window).scroll(function (){
-   var scroll = $(window).scrollTop();
-    $('.left').css({
-        width: (100 + scroll/5) + "%"
-    })
-    $('.right').css({
-        width: (100 + scroll/5) + "%",
-        left: scroll/50 + "%"
-    })
-    $('.text').css({
-        top: scroll/20 + "%"
-    })
-});
+let rellax = new Rellax(".rellax");
